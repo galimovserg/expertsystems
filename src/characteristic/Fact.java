a6eb1332285516ed93eac3b98f23d8a8217bf8ea
galimovserg <galimovserg@users.noreply.github.com>
2018-10-29 22:31:44 +0500

    Задокументировал классы Fact, MultiRule

--- src/characteristic/Fact.java
+++ src/characteristic/Fact.java
@@ -1,7 +1,11 @@
 package characteristic;
 
 import java.util.ArrayList;
-
+/**
+ * Характеристика со списком возможных значений.
+ * @author sergey
+ *
+ */
 class Fact{
 	private String name=new String();
 	private ArrayList<String> ValuesList;
@@ -9,6 +13,12 @@ class Fact{
 		this.name=name;
 		this.ValuesList = new ArrayList<String>();
 	}
+	/**
+	 * Добавляет новое значение, вернет true если добавление прошло успешно 
+	 * и false если такое значение уже есть.
+	 * @param value
+	 * @return
+	 */
 	boolean addValue(String value){
 		if(hasval(value)){
 			System.out.println("Такое значение уже есть!");
@@ -23,6 +33,12 @@ class Fact{
 	ArrayList<String> getValues(){
 		return null;
 	}
+	/**
+	 * Вернет true если значение val есть в списке значений этой характеристики, 
+	 * иначе вернет false.
+	 * @param val
+	 * @return
+	 */
 	boolean hasval(String val){
 		int len = ValuesList.size();
 		for(int i=0;i<len;i++){
@@ -33,6 +49,10 @@ class Fact{
 		}
 		return false;
 	}
+	/**
+	 * Выводит имя характеристики и ее возможные значения
+	 * в понятном виде в консоль.
+	 */
 	public void print() {
 		// TODO Auto-generated method stub
 		System.out.println(this.name);
